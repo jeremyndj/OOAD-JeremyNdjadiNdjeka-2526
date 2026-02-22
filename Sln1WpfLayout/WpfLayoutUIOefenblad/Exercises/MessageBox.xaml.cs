fa67cc5a32aee3f035c320468324b192795ecd3c
jeremyndj <jeremy.ndjadindjeka@student.odisee.be>
2026-02-22 23:10:22 +0100

    WpfLayoutUiOefenblad

---
+++ Sln1WpfLayout/WpfLayoutUIOefenblad/Exercises/MessageBox.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WpfLayoutUIOefenblad.Helpers;
+
+namespace WpfLayoutUIOefenblad.Exercises;
+
+[NavPage(title: "MessageBox", description: "Dialoogvensters", order: 10)]
+public partial class MessageBoxDialog : Page
+{
+    public MessageBoxDialog()
+    {
+        InitializeComponent();
+        MessageBox.Show("Wil je de gegevens opslaan?", "Bevestigen", MessageBoxButton.YesNo);
+        MessageBoxResult result = MessageBox.Show("De gegevens zijn opgeslagen.");
+    }
+}
