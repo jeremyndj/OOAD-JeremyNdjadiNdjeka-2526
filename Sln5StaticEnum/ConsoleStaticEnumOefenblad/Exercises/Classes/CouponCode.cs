0a3e4be65afd1c86e561ff50576409c378c1e2d5
jeremyndj <jeremy.ndjadindjeka@student.odisee.be>
2026-03-24 16:30:49 +0100

    ConsoleStaticEnumOefenblad

---
+++ Sln5StaticEnum/ConsoleStaticEnumOefenblad/Exercises/Classes/CouponCode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+// 1. Maak in "Exercises/Classes" een klasse "CouponCode":
+//   - private static string _couponRegex = @"^[A-Z]{3}\d{2}-[A-Z]{2}$";
+//   - property "Code" van type string.
+//   - property "IsGeldig" van type bool met alleen getter:
+//     gebruik Regex.IsMatch(...) om te controleren of Code geldig is
+//   - constructor met één parameter
+//
+// 2. Voeg vervolgens statische methode ControleerCode(string code) toe, die toelaat een gegeven code te controleren
+//
+// 3. Voeg tenslotte nog een statische methode Beschrijf(string code) toe: 
+//   - als de code geldig is, geef je een tekst terug in dit formaat:
+//     "Prefix=ABC, Nummer=12, Regio=DE"
+//   - als de code ongeldig is, geef je "ongeldige code" terug.
+namespace ConsoleStaticEnumOefenblad.Exercises.Classes
+{
+    internal class CouponCode
+    {
+        private static string _couponRegex = @"^[A-Z]{3}\d{2}-[A-Z]{2}$";
+        public string Code { get; set; }
+        public bool IsGeldig 
+        { 
+            get
+            { 
+                return Regex.IsMatch(Code, _couponRegex); 
+            }
+        }
+        public CouponCode(string code) 
+        {
+            Code = code;
+        }
+
+        public static bool ControleerCode(string code) 
+        { 
+            return Regex.IsMatch(code, _couponRegex);
+        }
+    }
+}
---
+++ Sln5StaticEnum/ConsoleStaticEnumOefenblad/Exercises/Classes/WorkshopDeelnemer.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleStaticEnumOefenblad.Exercises.Classes;
+
+// Gegeven in "Exercises/Classes" is een klasse "WorkshopDeelnemer" met volgende properties:
+//  - Naam (private setter)
+//  - IsAanwezig (private setter)
+//  - een constructor met parameters voor naam en aanwezigheidsstatus
+//    
+// 1. Voeg volgende static properties toe aan die klasse:
+//  - een static property AantalAangemaakt (private setter)
+//  - een static property AantalAanwezig (private setter)
+//
+// 2. Pas de constructor aan zodat de statische properties aangepast worden.
+//
+// 3. Voeg daarna een (object)methode ZetAfwezig() toe.
+//   - die methode moet de aanwezigheidsstatus aanpassen én het globale aantal aanwezigen correct houden
+internal class WorkshopDeelnemer
+{
+    public string Naam { get; set; }
+    public bool IsAanwezig { get; private set; }
+
+    public static int AantalAangemaakt { get; private set; }
+    public static int AantalAanwezig { get; private set; }
+
+    public WorkshopDeelnemer(string naam, bool isAanwezig)
+    {
+        Naam = naam;
+        IsAanwezig = isAanwezig;
+        AantalAangemaakt++;
+        if (isAanwezig) 
+        { 
+            AantalAanwezig++;
+        }
+
+    }
+
+    public void ZetAfwezig()
+    {
+        if (IsAanwezig)
+        {
+            AantalAanwezig--;
+        }
+        IsAanwezig = false;
+    }
+}
